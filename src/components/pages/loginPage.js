b10ce0149e77cdc9f554e7b496350bc9a92014ad
lukeharrison95 <harrison.luke95@hotmail.com>
2021-05-09 20:36:25 +0100

    Added redirct onto login page for logged in users

--- src/components/pages/loginPage.js
+++ src/components/pages/loginPage.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Textfield from "@material-ui/core/Textfield";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import { login } from "../../redux/actions/users/userActions";
+import { Redirect } from 'react-router';
 
 
 const LoginPage = () =>{
 
+    const user = useSelector( (state) => state.users.currentUser)
     const dispatch = useDispatch();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -22,41 +24,48 @@ const LoginPage = () =>{
         )
     }
     
-    return(
-        
-        <div>
-            <Container>
-                <form onSubmit={handleSubmit}>
-                <Textfield
-                    fullWidth
-                    placeholder="Username"
-                    onChange={(e) => setUsername(e.target.value)}
-                    label="Username"
-                    required
-                    margin="normal"
-                    variant="outlined"
-                />
-                <Textfield
-                    fullWidth
-                    placeholder="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    label="Password"
-                    required
-                    margin="normal"
-                    variant="outlined"
-                />
-                    <Button
-                    type="submit"
-                    fullWidth
-                    color="primary"
-                    variant="contained"
-                    >
-                    Submit
-                    </Button>
-                </form>
-            </Container>
-        </div>
-    )
+    if(user){
+        return <Redirect to="/" />
+    } else{
+
+        return(     
+            <div>
+                <Container>
+                    <form onSubmit={handleSubmit}>
+                    <Textfield
+                        fullWidth
+                        placeholder="Username"
+                        onChange={(e) => setUsername(e.target.value)}
+                        label="Username"
+                        required
+                        margin="normal"
+                        variant="outlined"
+                    />
+                    <Textfield
+                        fullWidth
+                        placeholder="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        label="Password"
+                        required
+                        margin="normal"
+                        variant="outlined"
+                    />
+                        <Button
+                        type="submit"
+                        fullWidth
+                        color="primary"
+                        variant="contained"
+                        >
+                        Submit
+                        </Button>
+                    </form>
+                </Container>
+            </div>
+        )
+
+    }
+
+    
 }
 
 export default LoginPage;
